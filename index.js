44182693c9b976e583eb8c1a291888112b69ae79
Adisie Fenta <adisiefenta23@gmail.com>
2024-10-13 12:53:33 +0300

    changes

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-require('dotenv').config()
-const express = require('express')
-const cookieParser = require('cookie-parser')
+require("dotenv").config();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 // socket
-const {
-    server,
-    app
-} = require('./api/socket/socketIo');
+const { server, app } = require("./api/socket/socketIo");
 // db connection handler
-const dbConnectionHandler = require("./api/db/dbConnection")
+const dbConnectionHandler = require("./api/db/dbConnection");
 
 // port
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 // settings
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
 // routes
-app.use("/api",require("./api/routes/usersRoute"))
+app.use("/api", require("./api/routes/usersRoute"));
 
 // server
 server.listen(PORT, async () => {
-    await dbConnectionHandler()
-    console.log("listening")
-})
+  await dbConnectionHandler();
+  console.log("listening");
+});
